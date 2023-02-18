80d9300f4b8d8358afa3ccc758b115708fcfa8d4
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2023-02-18 17:06:02 +0900

    나무

--- Assets/Scripts/InGame/Woods/ChestNut.cs
+++ Assets/Scripts/InGame/Woods/ChestNut.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class ChestNut : Wood
 {
+    public int nutCount = 5;
+    private float radius = 5f;
+
+    public override void OnBoom()
+    {
+        base.OnBoom();
+
+        int i = 0;
+        while (i < nutCount)
+        {
+            float nutPoX = Random.Range(transform.position.x - 10f, transform.position.x + 10f);
+            float nutPoY = Random.Range(transform.position.y - 10f, transform.position.y + 10f);
+            Vector2 nutPo = new Vector2(nutPoX, nutPoY);
+            if (Vector2.Distance(transform.position, nutPo) < radius)
+            {
+                Managers.Resource.Instantiate("Wood/Nut");
+                ++nutCount;
+            }
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
