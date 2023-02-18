d10b578e6e0c9db6d6ef1c2745175c4cee0d06a7
hegria <79516073+hegria@users.noreply.github.com>
2023-02-18 12:03:18 +0900

    Start Scene

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -54,7 +54,6 @@ public class Managers : MonoBehaviour
             DontDestroyOnLoad(go);
             s_instance = go.GetComponent<Managers>();
 
-            s_instance._data.Init();
             s_instance._pool.Init();
             s_instance._sound.Init();
         }
